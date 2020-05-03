ba50232075e524956f9add99f50de184fbce921c
ajmr182 <ajmr182@gmail.com>
2020-05-02 22:11:32 -0500

    Rescata llave

---
+++ TrucoVen/app/src/main/java/com/skynoff/trucoven/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.skynoff.trucoven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.skynoff.trucoven.ui.SingUpActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun OpenSingUpActivity(view: View) {
+
+
+    }
+}
