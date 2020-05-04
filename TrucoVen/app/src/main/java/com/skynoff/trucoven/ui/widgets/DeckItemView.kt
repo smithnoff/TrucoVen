40c337bf4cd6a92672f6e527dcbbe1b0af8c5b40
DevPractice <devpractice@MAC88E9FE5CDE06-CS.local>
2020-05-03 20:18:55 -0400

    adding flip animation

--- TrucoVen/app/src/main/java/com/skynoff/trucoven/ui/widgets/DeckItemView.kt
+++ TrucoVen/app/src/main/java/com/skynoff/trucoven/ui/widgets/DeckItemView.kt
@@ -2,11 +2,15 @@ package com.skynoff.trucoven.ui.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.skynoff.trucoven.R
 
 class DeckItemView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-
+    init {
+        LayoutInflater.from(context).inflate(R.layout.item_deck_view, this, true)
+    }
 }
