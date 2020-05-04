2969fe609d6561f2439755521b17ccbb92b29f02
DevPractice <devpractice@MAC88E9FE5CDE06-CS.local>
2020-05-03 22:56:40 -0400

    added numbers and pint

--- TrucoVen/app/src/main/java/com/skynoff/trucoven/ui/table/GameTableFragment.kt
+++ TrucoVen/app/src/main/java/com/skynoff/trucoven/ui/table/GameTableFragment.kt
@@ -1,13 +1,16 @@
 package com.skynoff.trucoven.ui.table
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.skynoff.trucoven.R
+import com.skynoff.trucoven.background.data.models.Deck
+import com.skynoff.trucoven.background.data.models.Marking
+import kotlinx.android.synthetic.main.fragment_game_table.*
 
 class GameTableFragment : Fragment() {
 
@@ -23,12 +26,35 @@ class GameTableFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        card_one.setNumberPint(
+            Deck(
+                2,
+                Marking.Gold(),
+                2
+            )
+        )
+        card_two.setNumberPint(
+            Deck(
+                5,
+                Marking.Sword(),
+                5
+            )
+        )
+        card_three.setNumberPint(
+            Deck(
+                7,
+                Marking.Coarse(),
+                7
+            )
+        )
+
+        card_one.setOnClickListener(listener)
+        card_two.setOnClickListener(listener)
+        card_three.setOnClickListener(listener)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home){
-            findNavController().navigateUp()
-        }
-        return super.onOptionsItemSelected(item)
+    private val listener = View.OnClickListener {
+        ViewCompat.setElevation(it, 12f);
     }
 }
